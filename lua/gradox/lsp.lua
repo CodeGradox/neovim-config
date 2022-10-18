d5b2ee8acf80d74cf1c1e1a457bee0834e704348
Magnus Åsrud <magnusasrud92@gmail.com>
2022-10-18 13:41:46 +0200

    Fix deprecation

--- lua/gradox/lsp.lua
+++ lua/gradox/lsp.lua
@@ -5,7 +5,7 @@ local nvim_lsp = require("lspconfig")
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local on_attach = function(client, bufnr)
   -- Shortcuts for keymaps and option setter.
