7734543581264810ca30782e26cb94fcf8db8dd8
Magnus Åsrud <magnusasrud92@gmail.com>
2022-11-20 15:32:57 +0100

    Updated colorscheme

--- lua/gradox/colorscheme.lua
+++ lua/gradox/colorscheme.lua
@@ -1,15 +1,8 @@
 -- Color schemes
 
-vim.opt.background = "dark"
+-- vim.opt.background = "dark"
+vim.opt.background = "light"
 vim.cmd [[colorscheme onehalf-lush]]
-
--- Disable nvim background
-vim.cmd [[hi Normal guibg=NONE]]
-
--- Colors for diff in vim-fugitive. Matches the onehalf-lush theme.
-vim.cmd [[hi diffAdded guifg=#50A14F guibg=NONE]]
-vim.cmd [[hi diffRemoved guifg=#E45649 guibg=NONE]]
-vim.cmd [[hi diffChanged guifg=#C18401 guibg=NONE]]
 
 -- Highlight unwanted spaces in bright red.
 vim.cmd [[hi Whitespace guifg=#FFFFFF guibg=#E45649]]
