c78378b509f9d13676968318e091ce27c9f099ae
Magnus Åsrud <magnusasrud92@gmail.com>
2023-02-27 11:10:26 +0100

    Added textobjects plugin for treesitter

--- lua/gradox/plugins/treesitter.lua
+++ lua/gradox/plugins/treesitter.lua
@@ -12,6 +12,10 @@ return {
   -- Enable treesitter. It"s still considered experimental.
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "RRethy/nvim-treesitter-endwise",
+    },
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup {
@@ -20,6 +24,32 @@ return {
           -- Enable better syntax highlights.
           enable = true,
           additional_vim_regex_highlighting = false,
+        },
+        textobjects = {
+          select = {
+            -- Better selection for objects, such as functions and classes.
+            enable = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["as"] = "@scope",
+            },
+          },
+          swap = {
+            -- Swap order of parameters.
+            enable = true,
+            swap_next = {
+              ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>A"] = "@parameter.inner",
+            },
+          },
         },
       }
     end
