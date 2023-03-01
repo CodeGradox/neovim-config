1568267fef2a986417ea96655cb61add003097e1
Magnus Åsrud <magnusasrud92@gmail.com>
2023-03-01 13:05:42 +0100

    Setup colorscheme in plugin config

--- lua/gradox/colorscheme.lua
+++
@@ -1,8 +0,0 @@
--- Color schemes
-
--- vim.opt.background = "dark"
-vim.opt.background = "light"
-vim.cmd [[colorscheme onehalf-lush]]
-
--- Highlight unwanted spaces in bright red.
-vim.api.nvim_set_hl(0, "Whitespace", { bg = "#e45649", fg = "#ffffff" })
--- lua/gradox/plugins/colorscheme.lua
+++ lua/gradox/plugins/colorscheme.lua
@@ -3,5 +3,13 @@ return {
     "CodeGradox/onehalf-lush",
     lazy = false,
     priority = 1000,
+    config = function()
+      -- vim.opt.background = "dark"
+      vim.opt.background = "light"
+      vim.cmd [[colorscheme onehalf-lush]]
+
+      -- Highlight unwanted spaces in bright red.
+      vim.api.nvim_set_hl(0, "Whitespace", { bg = "#e45649", fg = "#ffffff" })
+    end
   }
 }
