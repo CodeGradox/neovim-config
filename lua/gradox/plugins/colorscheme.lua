387552a43501795eea7646a4db2d1a554bc67be3
Magnus Åsrud <magnusasrud92@gmail.com>
2023-03-05 18:21:45 +0100

    Set light theme by default
    Can change it if neovim fixes the order in which plugins and ui are
    loaded.

--- lua/gradox/plugins/colorscheme.lua
+++ lua/gradox/plugins/colorscheme.lua
@@ -18,12 +18,14 @@ return {
         vim.cmd("colorscheme onehalf-lush-dark")
       end
 
+      set_light_mode()
+
       -- I think the background is set to dark by default.
-      if vim.api.nvim_get_option("background") == "dark" then
-        set_dark_mode()
-      else
-        set_light_mode()
-      end
+      -- if vim.api.nvim_get_option("background") == "dark" then
+      --   set_dark_mode()
+      -- else
+      --   set_light_mode()
+      -- end
 
       -- Highlight unwanted spaces in bright red.
       vim.api.nvim_set_hl(0, "Whitespace", { bg = "#e45649", fg = "#ffffff" })
@@ -32,7 +34,7 @@ return {
 
       -- Automatically set the colorscheme based on the macOS theme.
       auto_dark_mode.setup({
-        -- update_interval = 1000,
+        update_interval = 1000,
         set_dark_mode = set_dark_mode,
         set_light_mode = set_light_mode,
       })
