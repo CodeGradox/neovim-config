0275506a194a55232f9de929c670035f04734a4d
Magnus Åsrud <magnusasrud92@gmail.com>
2024-07-20 16:18:58 +0200

    Fix accidental nvim-tree folder navigation
    By pressing '-', I ended up moving up one directory when inside the
    nvim-tree buffer. The fix is do disable the '-' action when inside this
    buffer.

--- lua/gradox/plugins/nvim-tree.lua
+++ lua/gradox/plugins/nvim-tree.lua
@@ -1,3 +1,17 @@
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local noop = function() end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  -- Disable the - key to prevent me from accidentaly navigating to the parent directory.
+  vim.keymap.set('n', '-', noop, { buffer = bufnr, noremap = true, silent = true, nowait = true })
+  -- vim.keymap.set('n', '<Alt-->', function() print("Hello") end, { buffer = bufnr, noremap = true, silent = true, nowait = true })
+end
+
 return {
   -- File explorer for neovim.
   {
@@ -9,7 +23,8 @@ return {
       require("nvim-tree").setup {
         view = {
           width = 50
-        }
+        },
+        on_attach = my_on_attach
       }
     end
   }
