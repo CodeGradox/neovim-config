0420305ee54e191cc8aecfe9353197abe7c6a62f
Magnus Åsrud <magnusasrud92@gmail.com>
2024-07-28 18:14:59 +0200

    cmp-nvim lsp signature help

--- lua/gradox/plugins/00-plugins.lua
+++ lua/gradox/plugins/00-plugins.lua
@@ -8,6 +8,7 @@ return {
   "hrsh7th/cmp-nvim-lsp",
   "hrsh7th/cmp-buffer",
   "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp-signature-help",
 
   -- Snippets for cmp-nvim
   "saadparwaiz1/cmp_luasnip",
